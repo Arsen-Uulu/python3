a1af091d51cc65f9fc8d03481a927bf11ce5d08f
Arsen Sydykov <arsensydykov@770-lg-17473.jcrew.com>
2020-06-22 22:04:36 -0400

    Added delete method and check if the array if full,fixed repetitive of the code

--- complex_data_structure/hash_map.py
+++ complex_data_structure/hash_map.py
@@ -10,23 +10,33 @@ class HashMap:
 
   def compressor(self, hash_code):
     return hash_code % self.array_size
-
-  def assign(self, key, value):
+  
+  def delete(self,key):
     array_index = self.compressor(self.hash(key))
-    current_array_value = self.array[array_index]
+    possible_delete_key_value = self.array[array_index]
+    if (possible_delete_key_value[0] is None):
+      return 'Key not found'
 
-    if current_array_value is None:
-      self.array[array_index] = [key, value]
+    if (possible_delete_key_value[0] == key):
+      self.array.remove(possible_delete_key_value)
       return
-
-    if current_array_value[0] == key:
-      self.array[array_index] = [key, value]
-      return
-
-    # Collision!
-
+     
     number_collisions = 1
+    while(possible_delete_key_value[0] !=key):
+      new_hash_code = self.hash(key, number_collisions)
+      new_array_index = self.compressor(new_hash_code)
+      current_array_value = self.array[new_array_index]
 
+      if possible_delete_key_value is None:
+        return 'Key Not found'
+      
+      if possible_delete_key_value == key:
+        self.array.remove(possible_delete_key_value)
+        return
+      number_collisions += 1
+      return
+      
+  def check_collision(self,key):
     while(current_array_value[0] != key):
       new_hash_code = self.hash(key, number_collisions)
       new_array_index = self.compressor(new_hash_code)
@@ -43,30 +53,38 @@ class HashMap:
       number_collisions += 1
 
     return
+    
+  def assign(self, key, value):
+    array_index = self.compressor(self.hash(key))
+    current_array_value = self.array[array_index]
+
+    if current_array_value is None:
+      self.array[array_index] = [key, value]
+      return
+
+    if current_array_value[0] == key:
+      self.array[array_index] = [key, value]
+      return
+    
+    if (self.array_size == len(self.array)):
+      return 'Array is full'
+
+    # Collision!
+
+    number_collisions = 1
+
+    self.check_collistion(key)
 
   def retrieve(self, key):
     array_index = self.compressor(self.hash(key))
     possible_return_value = self.array[array_index]
 
     if possible_return_value is None:
-        return None
+      return None
 
     if possible_return_value[0] == key:
-        return possible_return_value[1]
+      return possible_return_value[1]
 
-    # possible_return_value holds a different key
-    if possible_return_value[0] != key:
-      retrieval_collisions = 1
-      while(possible_return_value[0] != key):
-        new_hash_code = self.hash(key,retrieval_collisions)
-        retrieving_array_index = self.compressor(new_hash_code)
-        possible_return_value = self.array[retrieving_array_index]
+    retrieval_collisions = 1
 
-        if possible_return_value is None:
-          return
-
-        if possible_return_value[0] == key:
-          return possible_return_value[1]
-          
-        retrieval_collisions += 1
-    return
+    self.check_collision(key)
